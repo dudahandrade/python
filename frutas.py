03b5a8ef3f54d6cb320c5f5f839c82afdc88d4ff
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-06-17 08:51:01 -0300

    Create frutas.py

---
+++ frutas.py
@@ -0,0 +1,9 @@
+listafrutas=["uva","morango","pera"]
+fruta= "morango"
+print(listafrutas(1))
+print(fruta(1))
+print(listafrutas[2][8])
+listafrutas.append("abacaxi")
+print(listafrutas)
+listafrutas.remove("pera")
+print(listafrutas)
