e307b3fcbbd346f21c2bfab7e4f01162ce109c47
dudahandrade <130777294+dudahandrade@users.noreply.github.com>
2024-07-02 10:56:18 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,17 +1,27 @@
-class conta:
-    def __init__(self):
-        print(self)
+def cria_conta(numero,titular,saldo,limite):
+    conta = {"numero":numero, "titular":titular,"saldo":saldo,"limite":limite}
+    return conta
 
+def saldo(conta):
+    print("conta numero:{}".format(conta["numero"]))
+    print("titular:"+conta["titular"])
+    print("saldo:{}".format(conta["saldo"]))
+def deposito(conta,valor):
 
+    conta["saldo"] +=valor
 
+def extrato (conta):
+    print("conta {} saldo {}". format(conta["numero"], conta ["saldo"]))
+def deposito (conta, valor):
+    conta["saldo"] += valor
+def sacar(conta, valor):
+    conta["saldo"] -= valor    
+    
+conta00= cria_conta(1234, "Arisberto", 50.0, 100.0)
+conta01= cria_conta(4321,"Leandro", 50.0,100.0)
 
+saldo(conta00)
+saldo(conta01)
 
-
-
-
-
-
-
-conta01 = conta()
-print(conta01)
+deposito(conta00, 100.0)
 
